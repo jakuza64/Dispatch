f9c27bc6e4d440f1cbad7886ba169556c03133a2
jakuza64 <mjako64@gmail.com>
2018-04-13 17:01:16 +0200

    clock and pause functions are in, bugs included

--- src/button.js
+++ src/button.js
@@ -4,26 +4,34 @@ function Button(text, callBack, isVisible, x, y) {
 	this.button = new PIXI.Container();
 
 	//creates the button's text object
-	let textObj = new Text(text,isVisible,1,x,y);
+	this.textObj = new Text(text,isVisible,1,x,y);
 
-	//creates the rectangle
-	let rect = new PIXI.Graphics();
-	//rect.isFastRect(true);
+	//creates the rectangle and adds to button container
+	this.rect = new PIXI.Graphics();
+	this.rect.isFastRect(true);
 	this.padding = 8;
-	let width = textObj.getWidth();//using text width and height to determine rectangle properties
-	let height = textObj.getHeight();
-	rect.lineStyle(0);
-	rect.beginFill(0xF0F3F3);
-	rect.drawRect(x-this.padding, y-this.padding, width+(this.padding*2), height+(this.padding*2));
-	rect.endFill();
-	//invisible by default
-	rect.visible = false;
-	this.button.addChild(rect);
-	//set button hit area to same as rectangle
-	this.button.hitArea = rect.getBounds();
 
-	//adds button functionality to rectangle
+	this.updateRect = function() {
+		let width = this.textObj.getWidth();//using text width and height to determine this.rectangle properties
+		let height = this.textObj.getHeight();
+		this.rect.clear();
+		this.rect.lineStyle(0);
+		this.rect.beginFill(0xF0F3F3);
+		this.rect.drawRect(x-this.padding, y-this.padding, width+(this.padding*2), height+(this.padding*2));
+		this.rect.endFill();
+
+		//set button hit area to same as this.rectangle
+		this.button.hitArea = this.rect.getBounds();
+	}
+
+	this.updateRect();
+	//invisible by default
+	this.rect.visible = false;
+	this.button.addChild(this.rect);	
+
+	//adds button functionality to button container
 	this.button.interactive = true;
+	this.button.interactiveChildren = false;
 	this.button.buttonMode = true;
 	this.button.on('pointerdown', function() {
 		callBack();
@@ -31,11 +39,20 @@ function Button(text, callBack, isVisible, x, y) {
 			.on('pointerover', onPointerOver)
 			.on('pointerout', onPointerOut);
 
-	//added after rectangle for sorting
-	this.button.addChild(textObj.textObj);
+	//added after this.rectangle for sorting
+	this.button.addChild(this.textObj.textObj);
 
 	//adds the button to the stage
 	app.stage.addChild(this.button);
+
+	this.setText = function(text, updateBounds) {
+		this.textObj.setText(text);
+		if (updateBounds) {
+			this.updateRect();
+		}
+	}
+
+	
 }
 
 function onPointerOver() {
